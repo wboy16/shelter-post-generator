8b65c9ad75a5fa13db5aee2531d914c2dafb3889
Mongreezy <gamefreak148d@gmail.com>
2026-06-30 15:06:49 +0200

    feat: implement profile pet creator with emojis and shelter copyright

---
+++ 3.pet-profile-creator.py
@@ -0,0 +1,9 @@
+# 3. Profile Pet Creator
+shelter_name = "©️SPCA"
+pet_name = input("What is your pet's name?\n")
+animal_type = input("What type of animal is your pet? (dog🐶, cat😺, etc.)\n")
+quirky_trait = input("What is a quirky trait your pet has?\n")
+
+print("Pet name: " + pet_name + "\nAnimal type: " + animal_type + "\nQuirky trait: " + quirky_trait)
+print("Meet " + pet_name + "! " + pet_name + " is a " + animal_type + " who " + quirky_trait +
+      ". Come adopt them today!\n" + shelter_name)
